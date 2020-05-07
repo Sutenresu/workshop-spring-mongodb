65275cdea6e506dc3c64c17657145f5b97848227
Sutenresu <joaovitor_g.s@hotmail.com>
2020-05-07 16:44:24 -0300

    Initial Commit

---
+++ src/main/java/com/educandoweb/workshopmongo/WorkshopSpringmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringmongodbApplication.class, args);
+	}
+
+}
